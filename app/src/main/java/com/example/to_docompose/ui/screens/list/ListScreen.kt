35c0af42356cfcbdc9a7e5c9c28966e0a0ac612b
ercilio.manhica <ercilio.manhica@wit-software.com>
2023-08-30 22:07:57 +0200

    Implement snackbar

--- app/src/main/java/com/example/to_docompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/list/ListScreen.kt
@@ -6,18 +6,24 @@ import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.example.to_docompose.R
 import com.example.to_docompose.ui.theme.fabBackground
 import com.example.to_docompose.ui.viewmodels.SharedViewModel
+import com.example.to_docompose.util.Action
 import com.example.to_docompose.util.SearchAppBarState
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -31,14 +37,25 @@ fun ListScreen(
     }
 
     val action by sharedViewModel.action
+
     val allTasks = sharedViewModel.allTasks.collectAsState()
     Log.d("ListScreen", "LaunchedEffect Trigged")
 
     val searchAppBarState: SearchAppBarState by sharedViewModel.searchAppBarState
     val searchTextState: String by sharedViewModel.searchTextState
 
-    sharedViewModel.handleDatabaseActions(action)
+
+    val scaffoldState = rememberScaffoldState()
+
+    DisplaySnackBar(
+        scaffoldState = scaffoldState,
+        handleDatabaseActions = { sharedViewModel.handleDatabaseActions(action = action) },
+        taskTitle = sharedViewModel.title.value,
+        action = action
+    )
+
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             ListAppBar(
                 sharedViewModel = sharedViewModel,
@@ -72,9 +89,28 @@ fun ListFab(
         )
     }
 }
-//
-//@Composable
-//@Preview
-//private fun ListScreenPreview() {
-//    ListScreen(navigateToTaskScreen = {})
-//}
+
+@Composable
+fun DisplaySnackBar(
+    scaffoldState: ScaffoldState,
+    handleDatabaseActions: () -> Unit,
+    taskTitle: String,
+    action: Action
+) {
+    handleDatabaseActions()
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = action) {
+        Log.d("ListScreen", "Snackbar")
+        Log.d("ListScreen", action.toString())
+        if (action != Action.NO_ACTION) {
+            scope.launch {
+               val result = scaffoldState.snackbarHostState.showSnackbar(
+                    message = "${action.name}: $taskTitle",
+                    actionLabel = "ok",
+                    duration = SnackbarDuration.Short
+                )
+            }
+        }
+    }
+}
